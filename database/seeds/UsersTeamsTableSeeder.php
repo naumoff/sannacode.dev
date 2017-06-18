3ed45741c5269bf1b4b59e93fdb8243320e5f311
Andrey Naumoff <andrey.naumoff@gmail.com>
2017-06-18 16:30:14 +0300

    seeder added for users and team_followers tables

--- database/seeds/UsersTeamsTableSeeder.php
+++ database/seeds/UsersTeamsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UsersTeamsTableSeeder extends Seeder
 {
@@ -12,5 +13,14 @@ class UsersTeamsTableSeeder extends Seeder
     public function run()
     {
         $users = factory(App\User::class,50)->create();
+        $teams = App\Team::all();
+        foreach ($users AS $key=>$user){
+        	DB::table('team_followers')->insert([
+        		'team_id'=>$teams[rand(0,count($teams)-1)]->id,
+		        'user_id'=>$user->id,
+		        'created_at'=>Carbon::now(),
+		        'updated_at'=>Carbon::now(),
+	        ]);
+        }
     }
 }
